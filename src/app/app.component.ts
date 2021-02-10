a3b4379c4accf0e9460a0970a84931056655e72d
fernando borba <fsborba5@gmail.com>
2021-02-09 21:55:46 -0300

    atualizando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cliente';
+
+  cards = [
+    {
+      image: 'https://i.pinimg.com/originals/de/f6/96/def69643889ee29e232637646e839064.jpg',
+      titulo: 'Card 1',
+      texto: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+    {
+      image: 'https://i2.wp.com/marketingcomcafe.com.br/wp-content/uploads/2017/12/banco-imagens-gratis.png',
+      titulo: 'Card 2',
+      texto: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+    {
+      image: 'https://portalfotografianapratica.com/wp-content/uploads/2019/09/miniatura-01-730x410.jpg',
+      titulo: 'Card 3',
+      texto: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+    {
+      image: 'https://www.designerd.com.br/wp-content/uploads/2019/04/imagens-blogs-chamar-atencao-publico-3.jpg',
+      titulo: 'Card 4',
+      texto: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+  ];
 }
